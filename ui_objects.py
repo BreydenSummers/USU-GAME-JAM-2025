26431420204f830b3ed7618f11d58f4ebcde378a
kadenpatten <kadenpatten52@gmail.com>
2025-01-24 19:35:35 -0700

    Add draw functions

--- ui_objects.py
+++ ui_objects.py
@@ -30,9 +30,12 @@ class RectButton(Clickable):
         super().__init__(self,cx,cy,w,h,color,callback,type="box")
 
     def draw(self,screen):
-        pygame.draw.rect(screen)
+        pygame.draw.rect(screen,self.color,(self.cx,self.cy,self.cw,self.ch))
 
 
 class RoundButton(Clickable):
-    def __init__(self,cx,cy,w,h,color,callback):
-        super().__init__(self,cx,cy,w,h,color,callback,type="circle")
+    def __init__(self,cx,cy,r,color,callback):
+        super().__init__(self,cx,cy,r,r,color,callback,type="circle")
+
+    def draw(self,screen):
+        pygame.draw.circle(screen,self.color,(self.cx,self.cy),self.w)
