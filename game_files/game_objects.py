c8009b052763db6e88fd7a988913aa72f8793e39
kadenpatten <kadenpatten52@gmail.com>
2025-01-25 00:50:33 -0700

    Update game objects

--- game_files/game_objects.py
+++ game_files/game_objects.py
@@ -5,8 +5,8 @@ TILE_SIZE = 8
 class NonInteractiveObject:
     def __init__(self,sprite,row,column):
         self.sprite = pygame.image.load(sprite).convert_alpha()
-        self.x = row * 8
-        self.y = column * 8
+        self.x = row * TILE_SIZE
+        self.y = column * TILE_SIZE
 
     def draw(self,screen):
         self.sprite.blit(screen,(self.x,self.y))
@@ -14,8 +14,8 @@ class NonInteractiveObject:
 class InteractiveObject:
     def __init__(self,sprite,row,column):
         self.sprite = pygame.image.load(sprite).convert_alpha()
-        self.x = row * 8
-        self.y = column * 8
+        self.x = row * TILE_SIZE
+        self.y = column * TILE_SIZE
 
     def draw(self,screen):
         self.sprite.blit(screen,(self.x,self.y))
